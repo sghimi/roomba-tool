5ba3034a0fb5b154cef2506ff5d4a95c060abdc6
Saugat Ghimire <sgdoger@gmail.com>
2023-04-03 20:09:49 +0000

    Update map.py

--- map.py
+++ map.py
@@ -5,12 +5,53 @@ import matplotlib.pyplot as plt
 with open('map.json') as f:
     data = json.load(f)
 
-# Extract the coverage layer coordinates
-layer = [l for l in data['umf']['maps'][0]['layers'] if l['layer_type'] == 'coverage'][0]
-coordinates = layer['geometry']['coordinates']
+border_nums = data['umf']['maps'][0]['borders'][0]['geometry']['ids']
+border_layer = data['umf']['maps'][0]['points2d']
 
-# Plot the coordinates
-x = [c[0] for c in coordinates]
-y = [c[1] for c in coordinates]
-plt.plot(x, y, 'o')
+# Create a dictionary mapping border IDs to coordinates
+id_to_coord = {}
+for point in border_layer:
+    id_to_coord[point['id']] = point['coordinates']
+
+# Get the coordinates associated with border_nums IDs
+border_coords = [id_to_coord[id] for id in border_nums[0]]
+
+
+
+
+# Loop through all layers in the map
+for layer in data['umf']['maps'][0]['layers']:
+    
+    # Check the layer type and get the corresponding coordinates
+    if layer['layer_type'] == 'coverage':
+        coverage_layer = layer['geometry']['coordinates']
+
+    elif layer['layer_type'] == 'coverage_poly':
+        coverage_poly_layer = layer['geometry']['coordinates']
+        coverage_poly_layer = coverage_poly_layer[0][0] # Get the first set of coordinates
+
+
+        print('Coverage poly layer coordinates:', coverage_poly_layer)
+
+print("cov layer\n\n",coverage_layer)
+print ("cov_poly\n\n",coverage_poly_layer)
+print("border \n\n",border_nums)
+print("border \n\n",border_coords)
+
+
+# Create a figure and axes
+fig, ax = plt.subplots()
+
+# Plot the border coordinates
+x, y = zip(*border_coords)
+ax.plot(x, y,label='Border')
+
+# Plot the coverage layer coordinates
+x, y = zip(*coverage_poly_layer)
+ax.scatter(x, y, label='Coverage')
+
+
+
+# Show the plot
 plt.show()
+
